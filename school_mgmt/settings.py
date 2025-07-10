8f461fec4498a83d8d8c6ced7afd81525548f765
Umer Qaisar <umer.qaisar@isoftstudios.com>
2025-07-10 17:35:28 +0500

    Added jwt auth settings

--- school_mgmt/settings.py
+++ school_mgmt/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'users',
     'students',
     'teachers',
@@ -55,6 +56,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
 
 ROOT_URLCONF = 'school_mgmt.urls'
 
